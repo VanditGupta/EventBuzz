34eeea11e3c50e870f86d1cc80f3c8a71ef1a493
VanditGupta <vanditgupta22@gmail.com>
2023-11-22 17:55:36 -0500

    Fixed drop statement

--- Code/SQL/EventBuzz.sql
+++ Code/SQL/EventBuzz.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS EventBuzz;
 USE EventBuzz;
 
--- DROP DATABASE IF NOT EXISTS EventBuzz;
+-- DROP DATABASE EventBuzz;
 
 CREATE TABLE IF NOT EXISTS Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -184,3 +184,4 @@ CREATE TABLE IF NOT EXISTS Events_OrganisedBy_Organisers (
     FOREIGN KEY (organiser_name)
         REFERENCES Organisers (organiser_name)
 );
+
