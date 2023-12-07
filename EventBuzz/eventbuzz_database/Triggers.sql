5ee1e21c8900a7e50a6c746afa12d0b1c407a692
VanditGupta <vanditgupta22@gmail.com>
2023-12-06 22:33:46 -0500

    Added triggers for Insert, Updated, Delete for Users, EventCategories and Venues tables

--- EventBuzz/eventbuzz_database/Triggers.sql
+++ EventBuzz/eventbuzz_database/Triggers.sql
@@ -2,7 +2,7 @@
 
 USE EventBuzz;
 
-
+-- Triggers to insert, update and delete the total_sponsorship_amount in Sponsors table
 DROP TRIGGER IF EXISTS after_sponsorship_insert;
 DELIMITER $$
 CREATE TRIGGER after_sponsorship_insert
@@ -55,7 +55,7 @@ BEGIN
 END$$
 DELIMITER ;
 
-
+-- Triggers to INSERT, UPDATE and DELETE the total_ticket_sales in Events table
 DROP TRIGGER IF EXISTS after_ticket_insert;
 DELIMITER $$
 CREATE TRIGGER after_ticket_insert
@@ -109,4 +109,141 @@ BEGIN
 END$$
 DELIMITER ;
 
+-- Triggers to add errors for Create, Read and Update for Users table in the ErrorLog table
+
+DROP TRIGGER IF EXISTS after_user_insert;
+DELIMITER $$
+CREATE TRIGGER after_user_insert
+AFTER INSERT ON Users
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_user_insert', 'Error occurred',CONCAT('user_id: ', NEW.user_id, ', user_name: ', NEW.username));
+    END;
+END$$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS after_user_update;
+DELIMITER $$
+CREATE TRIGGER after_user_update
+AFTER UPDATE ON Users
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_user_update', 'Error occurred',CONCAT('user_id: ', NEW.user_id, ', user_name: ', NEW.username));
+    END;
+END$$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS after_user_delete;
+DELIMITER $$
+CREATE TRIGGER after_user_delete
+AFTER DELETE ON Users
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_user_delete', 'Error occurred',CONCAT('user_id: ', OLD.user_id, ', user_name: ', OLD.username));
+    END;
+END$$
+DELIMITER ;
+
+-- Triggers to add errors for Create, Read and Update for EventCategories table in the ErrorLog table
+
+DROP TRIGGER IF EXISTS after_eventcategory_insert;
+DELIMITER $$
+CREATE TRIGGER after_eventcategory_insert
+AFTER INSERT ON EventCategories
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_eventcategory_insert', 'Error occurred',CONCAT('category_name: ', NEW.category_name));
+    END;
+END$$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS after_eventcategory_update;
+DELIMITER $$
+CREATE TRIGGER after_eventcategory_update
+AFTER UPDATE ON EventCategories
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_eventcategory_update', 'Error occurred',CONCAT('category_name: ', NEW.category_name));
+    END;
+END$$
+DELIMITER ;
+
+
+DROP TRIGGER IF EXISTS after_eventcategory_delete;
+DELIMITER $$
+CREATE TRIGGER after_eventcategory_delete
+AFTER DELETE ON EventCategories
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_eventcategory_delete', 'Error occurred',CONCAT('category_name: ', OLD.category_name));
+    END;
+END$$
+DELIMITER ;
+
+-- Triggers to add errors for Create, Read and Update for Venues table in the ErrorLog table
+
+DROP TRIGGER IF EXISTS after_venue_insert;
+DELIMITER $$
+CREATE TRIGGER after_venue_insert
+AFTER INSERT ON Venues
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_venue_insert', 'Error occurred',CONCAT('venue_name: ', NEW.venue_name));
+    END;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS after_venue_update;
+DELIMITER $$
+CREATE TRIGGER after_venue_update
+AFTER UPDATE ON Venues
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_venue_update', 'Error occurred',CONCAT('venue_name: ', NEW.venue_name));
+    END;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS after_venue_delete;
+DELIMITER $$
+CREATE TRIGGER after_venue_delete
+AFTER DELETE ON Venues
+FOR EACH ROW
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        INSERT INTO EventBuzzAudit.ErrorLog (error_source, error_message, error_context)
+        VALUES ('after_venue_delete', 'Error occurred',CONCAT('venue_name: ', OLD.venue_name));
+    END;
+END$$
+DELIMITER ;
+
+-- select * from `EventBuzzAudit`.`ErrorLog`;
 -- show TRIGGERS;
